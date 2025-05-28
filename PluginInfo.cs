3269d817c6e9347e1c182eeea9401fdc8d042e0c
make1unbanner <Monketaggersv2@gmail.com>
2025-05-28 20:14:42 +0200

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.placiclient.gorillatag.menutemplate";
+        public const string Name = "Planci Template";
+        public const string Description = "Created by Monkeytaggers_offiziel with love <3";
+        public const string Version = "1.0.0";
+    }
+}
